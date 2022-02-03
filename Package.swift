eeaca888bd225a1925d986ce6ee8e4f36dc91aa8
Vinitha Vijayan <vinitha.vijayan@hpe.com>
2022-02-02 16:02:36 -0800

    Upadated README, added Package and LICENSE

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Meridian",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "Meridian",
+            targets: ["Meridian"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Meridian",
+            url: "https://files.meridianapps.com/meridian-ios-sdk/meridian-ios-sdk-only-8.0.0.zip",
+            checksum: "9dbddd171362164c6855d473bc37b394668fb10412dc0dc06d28fceb50002411"
+        )
+    ]
+)
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # meridian-ios-sdk
+The Meridian iOS SDK has all the tools you’ll need to embed the Meridian Editor’s maps, turn-by-turn navigation, indoor location awareness, and notifications into your own custom iOS app.
+
+## Get Started
+https://docs.meridianapps.com/hc/en-us/articles/360040165413-The-iOS-SDK-Guide
