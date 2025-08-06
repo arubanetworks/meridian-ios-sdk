7d4d39877f62d066fbfe2d59d33c752c090c7703
Vinitha Vijayan <vinitha.vijayan@hpe.com>
2025-08-06 12:53:37 -0700

    11.2.0 SDK release

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Meridian",
-            url: "https://files.meridianapps.com/meridian-ios-sdk/meridian-ios-sdk-only-11.1.0.zip",
-            checksum: "c81e2a8849666740eb46a0d6f655a7232baa8614aef27b324ef3aaeb046c3a5d"
+            url: "https://files.meridianapps.com/meridian-ios-sdk/meridian-ios-sdk-only-11.2.0.zip",
+            checksum: "843c453951f014df7ca761f4db90646674dbeabc26b5745d9371de140ffbb61c"
         )
     ]
 )
